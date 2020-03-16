e65c5ccc11515055044ef6fa181da1ecf9372919
Phuong Doan <35623720+phuongdoan13@users.noreply.github.com>
2020-03-16 22:05:42 +1100

    Change String.isValid and add isValidInput()

--- calc/main.swift
+++ calc/main.swift
@@ -25,5 +25,20 @@ extension String{
 	var isOperator : Bool{
 		return OPERATORS.contains(self)
 	}
+	
+	var isValid : Bool{
+		return self.isInt || self.isOperator
+	}
 }
+
+
+func isValidInput(_ arr : [String]) -> Bool{
+	let isIntOp = !arr.map{ $0.isValid }.contains(false) // O(n) check if all elements are integer and operator element
+	let isOddLength = (arr.count > 1 && arr.count % 2 == 1) // The array must be odd in lenght OR...
+	let isSingleWithInt = (arr.count == 1 && arr[0].isInt) //... contains only and only one integer
+	return isIntOp && (isOddLength || isSingleWithInt)
+}
+
+
+
 
